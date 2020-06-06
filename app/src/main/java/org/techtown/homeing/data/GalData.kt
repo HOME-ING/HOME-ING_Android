851d0f2d6f95bfdcce25d242b85f56053855f36f
kangmin1012 <kangmin1012@naver.com>
2020-06-07 04:51:32 +0900

    #4 in progress 55%

---
+++ app/src/main/java/org/techtown/homeing/data/GalData.kt
@@ -0,0 +1,5 @@
+package org.techtown.homeing.data
+
+data class GalData(
+    val img : String
+)
---
+++ app/src/main/java/org/techtown/homeing/viewholder/GalVH.kt
@@ -0,0 +1,17 @@
+package org.techtown.homeing.viewholder
+
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.search_contents_item.view.*
+import org.techtown.homeing.R
+import org.techtown.homeing.data.GalData
+
+class GalVH (view : View) : RecyclerView.ViewHolder(view){
+
+    val img = view.findViewById<ImageView>(R.id.contents_img)
+    fun onBind(data : GalData){
+        Glide.with(itemView).load(data.img).into(img)
+    }
+}
