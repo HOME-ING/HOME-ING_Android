7852e8b07766285a425f8c89978143b284f1535b
kangmin1012 <kangmin1012@naver.com>
2020-06-07 00:28:26 +0900

    MainTap

---
+++ app/src/main/java/org/techtown/homeing/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package org.techtown.homeing.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.techtown.homeing.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
